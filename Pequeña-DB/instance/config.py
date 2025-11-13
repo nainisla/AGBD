e4956b31b344949242e37f15eff5da914c0b890a
nainisla <nainisla50@gmail.com>
2025-11-13 12:02:48 -0300

    Diciembre

--- Pequeña-DB/instance/config.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///salon_event.db'  
-    # Usa SQLite como base de datos
-    SQLALCHEMY_TRACK_MODIFICATIONS = False  
-    # Desactiva advertencias sobre el uso de memoria
-
-db = SQLAlchemy()
